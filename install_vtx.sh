c64ffd677e1b4d1fb243b243e2a37f7024131382
snokvist <joakim.snokvist@gmail.com>
2025-05-01 12:04:58 +0200

    Update install_vtx.sh

--- install_vtx.sh
+++ install_vtx.sh
@@ -8,6 +8,10 @@ echo "chmod +x on relevant files ..."
 chmod -R +x usr/bin*
 chmod -R +x etc/init.d/*
 
+echo "Stopping running services..."
+SSHPASS="12345" sshpass -e ssh -o StrictHostKeyChecking=no -t root@"$IP" 'killall -q alink_drone' 2>&1 | grep -v debug1
+SSHPASS="12345" sshpass -e ssh -o StrictHostKeyChecking=no -t root@"$IP" 'killall -q air_man' 2>&1 | grep -v debug1
+
 echo "Starting scp ..."
 SSHPASS="12345" sshpass -e scp -o StrictHostKeyChecking=no -O -v -r -p /etc/gs.key root@"$IP":/etc/drone.key 2>&1 | grep -v debug1
 SSHPASS="12345" sshpass -e scp -o StrictHostKeyChecking=no -O -v -r -p usr/* root@"$IP":/ 2>&1 | grep -v debug1
