b8894a7f860b701aac83ec40a727dc6f4b196827
snokvist <joakim.snokvist@gmail.com>
2025-05-01 13:17:10 +0200

    Update install_vtx.sh

--- install_vtx.sh
+++ install_vtx.sh
@@ -7,6 +7,8 @@ ssh-keygen -f '/home/radxa/.ssh/known_hosts' -R "$IP"
 echo "chmod +x on relevant files ..."
 chmod -R +x usr/bin*
 chmod -R +x etc/init.d/*
+chmod +x air_man_cmd.sh
+chmod +x video_mode_chooser.sh
 
 echo "Stopping running services..."
 SSHPASS="12345" sshpass -e ssh -o StrictHostKeyChecking=no -t root@"$IP" 'killall -q majestic' 2>&1 | grep -v debug1
@@ -14,11 +16,16 @@ SSHPASS="12345" sshpass -e ssh -o StrictHostKeyChecking=no -t root@"$IP" 'killal
 SSHPASS="12345" sshpass -e ssh -o StrictHostKeyChecking=no -t root@"$IP" 'killall -q air_man' 2>&1 | grep -v debug1
 
 echo "Starting scp ..."
-SSHPASS="12345" sshpass -e scp -o StrictHostKeyChecking=no -v -r -p usr/* root@"$IP":/usr/ 2>&1 | grep -v debug1
-SSHPASS="12345" sshpass -e scp -o StrictHostKeyChecking=no -v -r -p etc/* root@"$IP":/etc/ 2>&1 | grep -v debug1
+SSHPASS="12345" sshpass -e scp -o StrictHostKeyChecking=no -v -r -p vtx/usr/* root@"$IP":/usr/ 2>&1 | grep -v debug1
+SSHPASS="12345" sshpass -e scp -o StrictHostKeyChecking=no -v -r -p vtx/etc/* root@"$IP":/etc/ 2>&1 | grep -v debug1
+SSHPASS="12345" sshpass -e scp -o StrictHostKeyChecking=no -v -r -p air_man_cmd.sh root@"$IP":/usr/bin/ 2>&1 | grep -v debug1
 
 echo "Scp completed ... rebooting ... wait for reconnect..."
 SSHPASS="12345" sshpass -e ssh -o StrictHostKeyChecking=no -t root@"$IP" 'reboot' 2>&1 | grep -v debug1
+
+echo "Copying VRX files..."
+cp -f video_mode_chooser.sh /usr/bin/
+cp -f video_modes_imx* /etc/
 
 echo "Reconnecting in 25s..."
 # Visual countdown using a loop and printf
