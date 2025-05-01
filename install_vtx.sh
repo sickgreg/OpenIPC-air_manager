6295e64d9013bd49bb578bcad3b0a08d6c33a653
snokvist <joakim.snokvist@gmail.com>
2025-05-01 13:19:14 +0200

    Update install_vtx.sh

--- install_vtx.sh
+++ install_vtx.sh
@@ -5,10 +5,11 @@ IP="${1:-10.5.0.10}"
 ssh-keygen -f '/home/radxa/.ssh/known_hosts' -R "$IP"
 
 echo "chmod +x on relevant files ..."
-chmod -R +x usr/bin*
-chmod -R +x etc/init.d/*
+chmod -R +x vtx/usr/bin/*
+chmod -R +x vrx/usr/bin/*
 chmod +x air_man_cmd.sh
 chmod +x video_mode_chooser.sh
+
 
 echo "Stopping running services..."
 SSHPASS="12345" sshpass -e ssh -o StrictHostKeyChecking=no -t root@"$IP" 'killall -q majestic' 2>&1 | grep -v debug1
@@ -24,6 +25,7 @@ echo "Scp completed ... rebooting ... wait for reconnect..."
 SSHPASS="12345" sshpass -e ssh -o StrictHostKeyChecking=no -t root@"$IP" 'reboot' 2>&1 | grep -v debug1
 
 echo "Copying VRX files..."
+cp -f vrx/usr/bin/* /usr/bin/
 cp -f video_mode_chooser.sh /usr/bin/
 cp -f video_modes_imx* /etc/
 
